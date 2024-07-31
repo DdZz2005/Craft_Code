43b8e4394bbe5058ce1fd0072d7eae647eb1dc09
DdZz2005 <daniilka.zinchenko.05@mail.ru>
2024-08-01 00:11:05 +0300

    Добавлен проект. Реализованы окна с регистрацией и авторизацией.

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.content.Intent
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val registrationTextView: TextView = findViewById(R.id.Registration)
+        registrationTextView.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
