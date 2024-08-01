1a1139622ed48d074e728ebe2189c3432888d4b1
DdZz2005 <daniilka.zinchenko.05@mail.ru>
2024-08-01 14:41:21 +0300

    Добавил шаблон привязки ViewBinding

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -10,11 +10,17 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
+
+
+
         val registrationTextView: TextView = findViewById(R.id.Registration)
         registrationTextView.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 
 
--- app/src/main/java/com/example/myapplication/RegistrationActivity.kt
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+    }
+    fun onClick(view: View){
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
     }
 }
